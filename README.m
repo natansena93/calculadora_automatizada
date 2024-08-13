8f8813b17324d02979e14bfeffaae6055f298d10
natansena93 <natan-sena@outlook.com>
2024-08-13 16:14:08 -0300

    Create README.m
    Adiciona arquivo README.M

---
+++ README.m
@@ -0,0 +1,52 @@
+# Calculadora Simples em Python
+
+este projeto é uma calculadora simples escrita em python que permite ao usuario realizar operações matemáticas básicas entre dois números.
+
+# Funcionalidades
+
+A calculadora suporta as seguintes operações:
+
+- ADIÇÃO (+): SOMA DE DOIS NÚMEROS.
+- SUBTRAÇÃO (-): SUBTRAI O SEGUNDO NÚMERO DO PRIMEIRO.
+- MULTIPLICAÇÃO (*): MULTIPLICA DOIS NÚMEROS.
+- DIVISÃO (/): DIVIDE O PRIMEIRO NÚMERO PELO SEGUNDO.
+- PORCENTAGEM (%): CALCULA A PORCENTAGEM DO PRIMEIRO NÚMERO EM RELAÇÃO AO SEGUNDO.
+
+# Como usar 
+
+# Uso 
+
+O programa pedirá para inserir dois números e, em seguida, um operador.
+Digite o operador desejado (+,-,*,/,%) para realizar a operação.
+O resultado será exibido na tela.
+
+# Executando em script Shell
+
+Abra o terminal.
+Navegue até o diretório onde o arquivo script.sh está localizado.
+De permissão de execução ao script (se necessario) com o comando : 
+chmod+x script.sh
+
+Execute o script com o comando:
+./script.sh
+
+# uso: O script shell executará automaticamente o código python,permitindo que vocÊ use a calculadoraa da mesma forma.
+
+# Exemplo de uso
+
+Aqui esta um exemplo de como a calculadora funciona:
+
+Insira o primeiro número: 10 
+Insira o segundo número :5
+Digite o operador (+,-,*,/,%):
++
+Resultado : 15.0
+
+
+# Possiveis Melhorias
+ Adicionar mais operadores e funcionalidades.
+ Tratar erros para entradas invalidas.
+
+Autor
+
+este projeto foi criado por Natan Sena
